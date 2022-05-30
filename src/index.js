d96dc45fd00679df1eac4e967939cb6328b57b97
dawicoin <dawicoin@gmail.com>
2022-05-30 21:56:26 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+const DawicoinReactSdk = ({}) => {
+ 
+  return (
+    <div className="dawicoin-react-sdk">
+      dawicoin-react-sdk init
+    </div>
+  );
+}
+
+export default DawicoinReactSdk;
